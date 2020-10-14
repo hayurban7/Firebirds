b92be39adc356afd89413840161919086629356a
hayurban7 <hayurban@iu.edu>
2020-10-14 17:17:54 -0400

    hayleys ideas

---
+++ js/member.app.js
@@ -0,0 +1,39 @@
+var app = new Vue({
+    el: '#memberProfile',
+    data: {
+            Member_ID:"", 
+          First_Name:"", 
+          Last_Name:"", 
+          Title:"", 
+          Gender:"", 
+          MemberStreet:"", 
+          MemberCity:"", 
+          MemberState:"", 
+          MemberZipCode:"", 
+          MemberPhone:"", 
+          Radio:"", 
+          Station:"", 
+          IsActive:"",
+    },
+    created() {
+        this.fetchUser();
+    },
+    methods: {
+        fetchUser: function() {
+            fetch('api/members/post.php')
+            .then(response => response.json())
+            .then(data => {
+                var userData = data.results[0];
+                console.log(Members);
+                this.memberID = Members.Member_ID;
+                this.memberFirst = Members.First_Name;
+                this.memberLast = Members.Last_Name;
+                this.memberTitle = Members.Title;
+                this.memberGender = Members.Gender;
+                this.userDate = Members.dob.date;
+                this.userLocation = userData.location.country;
+            }
+            );
+        },
+    }
+})
